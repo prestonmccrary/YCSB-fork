70822162bf26c3ae5622d7ef095c29f4bc23df54
Preston McCrary <prestonmccrary@berkeley.edu>
2024-04-18 00:44:57 -0700

    Catalog Boiler Working

---
+++ catalog/src/main/java/site/ycsb/db/CatalogClient.java
@@ -0,0 +1,101 @@
+package site.ycsb.db;
+
+import site.ycsb.ByteArrayByteIterator;
+import site.ycsb.ByteIterator;
+import site.ycsb.DB;
+import site.ycsb.DBException;
+import site.ycsb.Status;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Vector;
+
+public class CatalogClient extends DB {
+
+
+
+//  @Override
+//  public void init() throws DBException {
+//    System.out.println("Init");
+//  }
+//
+//  @Override
+//  public void cleanup() throws DBException {
+//    System.out.println("cleanup");
+//  }
+
+
+  /**
+   * Read a record from the database. Each field/value pair from the result will be stored in a HashMap.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to read.
+   * @param fields The list of fields to read, or null for all of them
+   * @param result A HashMap of field/value pairs for the result
+   * @return The result of the operation.
+   */
+  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result){
+    return Status.OK;
+  }
+
+  /**
+   * Perform a range scan for a set of records in the database. Each field/value pair from the result will be stored
+   * in a HashMap.
+   *
+   * @param table The name of the table
+   * @param startkey The record key of the first record to read.
+   * @param recordcount The number of records to read
+   * @param fields The list of fields to read, or null for all of them
+   * @param result A Vector of HashMaps, where each HashMap is a set field/value pairs for one record
+   * @return The result of the operation.
+   */
+  @Override
+  public Status scan(String table, String startkey, int recordcount, Set<String> fields,
+                              Vector<HashMap<String, ByteIterator>> result){
+    return Status.OK;
+  }
+
+  /**
+   * Update a record in the database. Any field/value pairs in the specified values HashMap will be written into the
+   * record with the specified record key, overwriting any existing values with the same field name.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to write.
+   * @param values A HashMap of field/value pairs to update in the record
+   * @return The result of the operation.
+   */
+  @Override
+  public Status update(String table, String key, Map<String, ByteIterator> values){
+    return Status.OK;
+  }
+
+  /**
+   * Insert a record in the database. Any field/value pairs in the specified values HashMap will be written into the
+   * record with the specified record key.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to insert.
+   * @param values A HashMap of field/value pairs to insert in the record
+   * @return The result of the operation.
+   */
+  @Override
+  public Status insert(String table, String key, Map<String, ByteIterator> values){
+
+    return Status.OK;
+
+  }
+
+  /**
+   * Delete a record from the database.
+   *
+   * @param table The name of the table
+   * @param key The record key of the record to delete.
+   * @return The result of the operation.
+   */
+  @Override
+  public Status delete(String table, String key){
+    return Status.OK;
+  }
+
+}
